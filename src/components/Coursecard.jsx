2d28158d8364740df50dcbefc2cbef2541b0ef08
kunalsonwane77 <kunalsonwane77@gmail.com>
2025-01-13 13:47:53 +0530

    code updated

--- src/components/Coursecard.jsx
+++ src/components/Coursecard.jsx
@@ -18,8 +18,14 @@ function Coursecard({title,Duration,status,instructor,id}) {
          
         <h1 className='font-medium text-2xl mt-2 p-3'>{title}</h1>
         <p className='m-3'> Duration : {Duration}</p>
+
+
         <button onClick={handlestatus} className={`border-2 px-2 py-1 m-3 text-white rounded-md ${status=="Active"?"bg-green-500":"bg-amber-500"}`}>{status}</button>
+
+
         <h3 className='text-xl m-2'>Instructor : {instructor}</h3>
+
+        
         <Link to={`/${id}`}><button className='border-2 py-1 px-2 m-1 bg-slate-600 text-white rounded-md'>Update</button></Link>
         <button onClick={()=>Dispatch({type:"delete",payload:id})} className='border-2 bg-red-400 text-white py-1 px-2 m-1 rounded-md'>Delete</button>
     </div>
